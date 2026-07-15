d6f4c2fae61a5af0dd09bb56e1563fb00e88178c
novabimperm-creator <roshior.a@pm-nova.ru>
2026-07-15 16:11:09 +0500

    new UI release

--- SSNumbererWPF.xaml.cs
+++ SSNumbererWPF.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using TNovCommon;
 
 namespace TNovSS
 {
@@ -12,32 +14,25 @@ namespace TNovSS
             InitializeComponent();
             textBox1.Focus();
             DataContext = viewModel;
-            
-            this.SizeToContent = SizeToContent.Height;
-            
-        }
-        /*
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Hide();
-        }
-        */
 
+            this.SizeToContent = SizeToContent.Height;
+        }
 
         private void escButton_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = false;
-            this.Close(); // закрытие окна
+            this.Close();
         }
 
-        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
         }
 
         private void HelpButton_Click(object sender, RoutedEventArgs e)
         {
-            string commandText = @"https://portal.talan.group/knowledge/proektirovanie/";
+            string commandText = HelpLinks.GetHelpLink("-");
             var proc = new System.Diagnostics.Process();
             proc.StartInfo.FileName = commandText;
             proc.StartInfo.UseShellExecute = true;
